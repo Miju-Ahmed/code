93e1fe438647730b1ca25bb381a165caeef8f83f
Miju <miju.ch7@gmail.com>
2023-02-16 12:15:56 +0600

    Bisection Method and Newton-Raphson Completely finished

---
+++ .history/Algorithm_Class/String/Maximum_Sum_20230216121519.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+        cin>>t;
+        while
+    return 0;
+}
---
+++ .history/Algorithm_Class/String/Maximum_Sum_20230216121535.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve
+
+int main()
+{
+    int t;
+        cin>>t;
+        while(t--)
+        solve();
+    return 0;
+}
